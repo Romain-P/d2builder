cae5caa032fdfd71953fd9fcffe1dccbd6a26611
Snapiz <snapizz97@gmail.com>
2015-06-16 19:00:04 -0700

    customDataWrapper implemented

--- app.js
+++ app.js
@@ -17,8 +17,10 @@ if(!program.output || !program.src) {
     program.outputHelp();
 }
 
+/*
 enumCvt.writeFiles(path.join(program.src, constants.src.enum), path.join(program.output, constants.output.enum));
 enumCvt.writeFile(path.join(program.src, constants.src.metadata), path.join(program.output, constants.output.metadata));
 enumCvt.writeFile(path.join(program.src, constants.src.protocolConstants), path.join(program.output, constants.output.protocolConstants));
 typeCvt.writeFiles(path.join(program.src, constants.src.type), path.join(program.output, constants.output.type));
 messageCvt.writeFiles(path.join(program.src, constants.src.message), path.join(program.output, constants.output.message));
+*/
