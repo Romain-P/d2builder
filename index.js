ef782021b9ba66f8997afc7e930d9ef3b95c761f
Snapiz <snapizz97@gmail.com>
2015-06-21 01:02:37 -0700

    write type

--- index.js
+++ index.js
@@ -14,11 +14,3 @@ if(!program.output || !program.src) {
 }
 
 d2builder(program.src, program.output);
-
-/*enumCvt.writeFiles(path.join(program.src, constants.src.enum), path.join(program.output, constants.output.enum));
-enumCvt.writeFile(path.join(program.src, constants.src.metadata), path.join(program.output, constants.output.metadata));
-enumCvt.writeFile(path.join(program.src, constants.src.protocolConstants), path.join(program.output, constants.output.protocolConstants));
-typeCvt.writeFiles(path.join(program.src, constants.src.type), path.join(program.output, constants.output.type));
-messageCvt.writeFiles(path.join(program.src, constants.src.message), path.join(program.output, constants.output.message));
-fs.copySync(path.join(__dirname, constants.src.protocolTypeManager), path.join(program.output, constants.output.protocolTypeManager));
-fs.copySync(path.join(__dirname, constants.src.messageReceiver), path.join(program.output, constants.output.messageReceiver));*/
